eb314b35c3c9bc3c598afaab791faf1020a540f8
Krzysztof Kabat <odrinwhite@hotmail.com>
2015-01-25 04:21:13 +0000

    sync

--- test/test.util.js
+++ test/test.util.js
@@ -24,20 +24,20 @@ describe('util', function() {
                 }
             }
             var dst = {}
-            util.copyAndEscape(object, dst, function(path){
+            util.copyAndEscape(object, dst, function(path) {
                 expect(path).to.eql(['doSth'])
                 done()
             })
-            
+
             expect(dst).to.eql({
-                    position: {
-                        x: 3,
-                        y: 5
-                    },
-                    text: 'test'
-                })
+                position: {
+                    x: 3,
+                    y: 5
+                },
+                text: 'test'
+            })
         })
-        
+
         it('should return correct object with two functions', function(done) {
 
             var object = {
@@ -51,32 +51,117 @@ describe('util', function() {
                     object.text = 'hello world'
                     console.log('do called')
                 },
-                make:function() {
-                    
+                make: function() {
+
                 }
             }
             var dst = {}
             var times = 0
-            util.copyAndEscape(object, dst, function(path){
-                if(times === 0){
+            util.copyAndEscape(object, dst, function(path) {
+                if (times === 0) {
                     expect(path).to.eql(['doSth'])
-                }else if(times  ===1){
+                }
+                else if (times === 1) {
                     expect(path).to.eql(['make'])
-                    done()   
+                    done()
                 }
                 times++
             })
-            
+
             expect(dst).to.eql({
-                    position: {
-                        x: 3,
-                        y: 5
-                    },
-                    text: 'test'
-                })
+                position: {
+                    x: 3,
+                    y: 5
+                },
+                text: 'test'
+            })
         })
         
-          it('should return correct object with two functions when one is in deep object', function(done) {
+        it('should return correct object with simple array', function(done) {
+
+            var object = {
+                position: {
+                    x: 3,
+                    y: 5
+                },
+                players : ['player1','player2'],
+                text: 'test',
+                doSth: function() {
+                    object.position.x = 5
+                    object.text = 'hello world'
+                    console.log('do called')
+                }
+            }
+            var dst = {}
+            util.copyAndEscape(object, dst, function(path) {
+                expect(path).to.eql(['doSth'])
+                done()
+            })
+            expect(dst).to.eql({
+                position: {
+                    x: 3,
+                    y: 5
+                },
+                text: 'test',
+                players : ['player1','player2']
+            })
+        })
+        
+        it('should return correct object with two functions and array', function(done) {
+
+            var object = {
+                position: {
+                    x: 3,
+                    y: 5
+                },
+                players : [{
+                    name : 'name1',
+                    move : function(){}
+                },{
+                    name : 'name2',
+                    move : function(){}
+                }],
+                text: 'test',
+                doSth: function() {
+                    object.position.x = 5
+                    object.text = 'hello world'
+                    console.log('do called')
+                },
+                make: function() {
+
+                }
+            }
+            var dst = {}
+            var times = 0
+            util.copyAndEscape(object, dst, function(path) {
+                if (times === 0) {
+                    expect(path).to.eql(['players','0', 'move'])
+                }
+                else if (times === 1) {
+                    expect(path).to.eql(['players','1', 'move'])
+                }
+                else if (times === 2) {
+                    expect(path).to.eql(['doSth'])
+                    done()
+                }
+                times++
+            })
+
+            expect(dst).to.eql({
+                position: {
+                    x: 3,
+                    y: 5
+                },
+                text: 'test',
+                players : [{
+                    name : 'name1'
+                },{
+                    name : 'name2'
+                }]
+            })
+        })
+
+        it('should return correct object with two functions when one is in deep object', function(done) {
 
             var object = {
                 position: {
@@ -89,39 +174,41 @@ describe('util', function() {
                     object.text = 'hello world'
                     console.log('do called')
                 },
-                level1:{
-                    level2:{
-                        make:function() {
-                    
+                level1: {
+                    level2: {
+                        make: function() {
+
                         }
                     }
                 }
             }
             var dst = {}
             var times = 0
-            util.copyAndEscape(object, dst, function(path){
-                if(times === 0){
+            util.copyAndEscape(object, dst, function(path) {
+                if (times === 0) {
                     expect(path).to.eql(['doSth'])
-                }else if(times  ===1){
-                    expect(path).to.eql(['level1', 'level2','make'])
-                    done()   
+                }
+                else if (times === 1) {
+                    expect(path).to.eql(['level1', 'level2', 'make'])
+                    done()
                 }
                 times++
             })
-            
+
             expect(dst).to.eql({
-                    position: {
-                        x: 3,
-                        y: 5
-                    },
-                    text: 'test',
-                    level1 :{
-                        level2: {
-                            
-                        }
+                position: {
+                    x: 3,
+                    y: 5
+                },
+                text: 'test',
+                level1: {
+                    level2: {
+
                     }
-                })
-        
+                }
+            })
+
         })
-    })
+
+    }) 
 })
